ba22a8a64e3be336f170463971728dbf066a6512
Benson Imoh,ST <benson@stbensonimoh.com>
2019-07-06 15:11:02 +0100

    Create sample configuration file

---
+++ configsample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is the Server configuration script
+ *
+ * PHP version 7.2
+ *
+ * @category Server_Configuration
+ * @package  Server_Configuration
+ * @author   Benson Imoh,ST <benson@stbensonimoh.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://stbensonimoh.com
+ */
+// Database Configuration
+$host='localhost';
+$db = '';
+$username = '';
+$password = '';
+// SMS Configuration
+$smstoken = '';
